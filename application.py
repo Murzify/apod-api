5a20561bfb3676b7dbc3fc3910ecefd3e748010c
Murzify <murzify@gmail.com>
2023-06-08 18:44:29 +0500

    heroku port

--- application.py
+++ application.py
@@ -327,4 +327,5 @@ def application_error(e):
 
 
 if __name__ == '__main__':
-    application.run('0.0.0.0', port=80)
+    port = int(os.environ.get("PORT"))
+    application.run('0.0.0.0', port=port)
